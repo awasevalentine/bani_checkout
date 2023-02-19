acb9fef67ba814a768003f80b22974ca5223ab40
awasevalentine <awasevalentine@gmail.com>
2023-02-19 19:07:48 +0100

    Adding the footer section

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import {TbLock } from 'react-icons/tb'
+const Footer = () => {
+    return ( 
+        <div>
+            <div className="flex w-[191.66px] rounded-[5px] bg-[rgba(255, 255, 255, 0.05)] justify-center items-center gap-[12px] flex-row">
+            <TbLock />
+            </div>
+        </div>
+     );
+}
+ 
+export default Footer;
